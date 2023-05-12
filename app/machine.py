129e802cc7d207c6b27a23490811cf1223e438b2
Ara Vartomian <aravartomian@gmail.com>
2023-05-11 23:09:18 -0700

    defined machine class

--- app/machine.py
+++ app/machine.py
@@ -1,2 +1,14 @@
 from sklearn.ensemble import RandomForestClassifier
 
+class Machine:
+    """A machine learning class to fit Random Forest Classifier.
+    Attributes
+    ----------
+    df: DataFrame 
+        a pandas dataframe object
+    Methods
+    ----------
+    save(filepath)
+    open(filepath)
+    info()
+"""
